23fe769f8d5ed36ab83f21ad1c54e8820bf695c0
Sonny Svensson <sonny.svensson@sdsab.se>
2020-05-21 00:51:42 +0200

    Register page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input 
+      type="email"
+      name="email"
+      placeholder="email"
+      v-model="email"
+      />
+      <input 
+      type="password"
+      name="password"
+      placeholder="password"
+      v-model="password"
+      />
+      <button 
+      @click="register">Register</button>
+  </div>
+  
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'register',
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+
+  
+  // ,
+  // mounted(){
+  //   setTimeout(() => {
+  //     this.email = 'Hello world'
+  //   }, 2000)
+    
+  // },
+  // watch:{
+  //   email (value) {
+  //     console.log('email has changed to ', value)
+  //   }
+  // }
+
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
